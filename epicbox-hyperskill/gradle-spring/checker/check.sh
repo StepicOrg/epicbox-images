5b2c2b2cb920eea4f983cd8c6721f56e4b9cfba6
Vladimir Turov <aaaaaa2493@yandex.ru>
2021-09-01 22:25:25 +0300

    Update hyperskill/gradle-spring profile

--- epicbox-hyperskill/gradle-spring/checker/check.sh
+++ epicbox-hyperskill/gradle-spring/checker/check.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [[ -d /home/gradle/.gradle_cache ]]; then
-    mv /home/gradle/.gradle_cache/* /home/gradle/.gradle
-    rm -rf /home/gradle/.gradle_cache
-fi
-
 cp -R /sandbox/. /checker/sandbox/files
 gradle test -q --project-dir=/checker/sandbox --no-daemon --console=plain --offline > stdout.txt 2> stderr.txt
 echo $? > code.txt
