618cdc959ea7b43abccdc9eee34fadaab7e8df57
Vladimir Turov <aaaaaa2493@yandex.ru>
2021-12-02 01:36:31 +0300

    Revert saving gradle cache

--- epicbox-hyperskill/gradle/checker/check.sh
+++ epicbox-hyperskill/gradle/checker/check.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ -d /home/gradle/.gradle_cache ]]; then
+    mv /home/gradle/.gradle_cache/* /home/gradle/.gradle
+    rm -rf /home/gradle/.gradle_cache
+fi
+
 cp -R /checker/template/. /sandbox
 python3 /checker/prepare_gradle.py
 gradle test -q --project-dir=/sandbox --no-daemon --console=plain --offline > stdout.txt 2> stderr.txt
