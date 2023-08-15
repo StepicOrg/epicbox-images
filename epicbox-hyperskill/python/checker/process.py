b2c45d470e3ffc57f171dc1e0debbf55d847467b
Sergey <polischuk0107@gmail.com>
2023-08-15 17:09:17 +0300

    Update process.py

--- epicbox-hyperskill/python/checker/process.py
+++ epicbox-hyperskill/python/checker/process.py
@@ -1,3 +1,45 @@
+# import json
+#
+# FAILED_TEST_BEGIN = '#educational_plugin FAILED + '
+# FAILED_TEST_CONTINUE = '#educational_plugin '
+#
+# if __name__ == '__main__':
+#     score = 1
+#     feedback = ''
+#
+#     with open('code.txt') as f:
+#         code = f.read().strip()
+#     with open('stdout.txt') as f:
+#         stdout = f.read().strip()
+#     with open('stderr.txt') as f:
+#         stderr = f.read().strip()
+#
+#     if code != '0':
+#         score = 0
+#         if any(line.startswith(FAILED_TEST_BEGIN) for line in stdout):
+#             output = []
+#             output_started = False
+#
+#             for line in stdout:
+#                 if output_started and line.startswith(FAILED_TEST_CONTINUE):
+#                     output.append(line[len(FAILED_TEST_CONTINUE):])
+#
+#                 if not output_started and line.startswith(FAILED_TEST_BEGIN):
+#                     output_started = True
+#                     output.append(line[len(FAILED_TEST_BEGIN):])
+#
+#             feedback = '\n'.join(output).strip()
+#         else:
+#             feedback = (
+#                 'stdout:\n{stdout}\n\nstderr:\n{stderr}'
+#                 .format(stdout='\n'.join(stdout), stderr='\n'.join(stderr))
+#             )
+#
+#     result = {
+#         'score': score,
+#         'feedback': feedback,
+#     }
+#     print(json.dumps(result))
 import json
 
 FAILED_TEST_BEGIN = '#educational_plugin FAILED + '
@@ -7,19 +49,22 @@ if __name__ == '__main__':
     score = 1
     feedback = ''
 
-    with open('code.txt') as f:
-        code = f.read().strip()
-    with open('stdout.txt') as f:
-        stdout = f.read().strip()
-    with open('stderr.txt') as f:
-        stderr = f.read().strip()
-
+    code = open('code.txt').read().strip()
+    stdout = open('stdout.txt').read().splitlines()
+    stderr = open('stderr.txt').read().splitlines()
     if code != '0':
         score = 0
-        if any(line.startswith(FAILED_TEST_BEGIN) for line in stdout):
+        if code != '1':
+            feedback = (
+                'Cannot check the submission.\n\nPerhaps your program '
+                'has fallen into an infinite loop or created too many objects in memory. '
+                'If you are sure that this is not the case, please send the report to support@hyperskill.org\n'
+                'stdout:\n{stdout}\n\nstderr:\n{stderr}'
+                .format(stdout='\n'.join(stdout), stderr='\n'.join(stderr))
+            )
+        elif any(line.startswith(FAILED_TEST_BEGIN) for line in stdout):
             output = []
             output_started = False
-
             for line in stdout:
                 if output_started and line.startswith(FAILED_TEST_CONTINUE):
                     output.append(line[len(FAILED_TEST_CONTINUE):])
@@ -30,11 +75,8 @@ if __name__ == '__main__':
 
             feedback = '\n'.join(output).strip()
         else:
-            feedback = (
-                'stdout:\n{stdout}\n\nstderr:\n{stderr}'
-                .format(stdout='\n'.join(stdout), stderr='\n'.join(stderr))
-            )
-
+            feedback = ('stdout:\n{stdout}\n\nstderr:\n{stderr}'
+                        .format(stdout='\n'.join(stdout), stderr='\n'.join(stderr)))
     result = {
         'score': score,
         'feedback': feedback,
