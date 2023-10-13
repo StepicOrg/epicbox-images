ed4a484bf5f1a39ae4d69d763ceef7237f088e44
Sergey <polischuk0107@gmail.com>
2023-10-13 10:56:15 +0300

    fixed the feedback when the tests are failing (#8)
    * fixed the feedback when the tests are failing
    
    * Update epicbox-hyperskill/python/checker/process.py
    
    Co-authored-by: Alexander Petrov <meanmail@meanmail.ru>
    
    * Update epicbox-hyperskill/python/checker/process.py
    
    Co-authored-by: Alexander Petrov <meanmail@meanmail.ru>
    
    * fixed the feedback when the tests are failing
    
    * fixed the feedback when the tests are failing
    
    * Update epicbox-hyperskill/python/checker/process.py
    
    Co-authored-by: Alexander Petrov <meanmail@meanmail.ru>
    
    ---------
    
    Co-authored-by: Alexander Petrov <meanmail@meanmail.ru>

--- epicbox-hyperskill/python/checker/process.py
+++ epicbox-hyperskill/python/checker/process.py
@@ -2,6 +2,8 @@ import json
 
 FAILED_TEST_BEGIN = '#educational_plugin FAILED + '
 FAILED_TEST_CONTINUE = '#educational_plugin '
+OK_CODE = '0'
+ASSERTION_ERROR_LINE = 'AssertionError: '
 
 if __name__ == '__main__':
     score = 1
@@ -10,25 +12,27 @@ if __name__ == '__main__':
     code = open('code.txt').read().strip()
     stdout = open('stdout.txt').read().splitlines()
     stderr = open('stderr.txt').read().splitlines()
-    if code != '0':
+    if code != OK_CODE:
         score = 0
-        feedback = (
-            'Cannot check the submission.\n\nPerhaps your program '
-            'has fallen into an infinite loop or created too many objects in memory. '
-            'If you are sure that this is not the case, please send the report to support@hyperskill.org\n'
-            'stdout:\n{stdout}\n\nstderr:\n{stderr}'
-            .format(stdout='\n'.join(stdout), stderr='\n'.join(stderr))
-        )
-        if stderr[0] == 'F':
-            for line in stderr:
-                if line.startswith('F'):
-                    continue
-                output.append(line)
-            feedback = '\n'.join(output).strip()
+        for line_index, line in enumerate(stderr):
+            if line.startswith(ASSERTION_ERROR_LINE):
+                feedback = '\n'.join([
+                    line[len(ASSERTION_ERROR_LINE):],
+                    *stderr[line_index + 1:]
+                ]).strip()
+                break
+        if not feedback:
+            feedback = (
+                'Cannot check the submission.\n\nPerhaps your program '
+                'has fallen into an infinite loop or created too many objects in memory. '
+                'If you are sure that this is not the case, please send the report to support@hyperskill.org\n'
+                'stdout:\n{stdout}\n\nstderr:\n{stderr}'
+                .format(stdout='\n'.join(stdout), stderr='\n'.join(stderr))
+            )
+
     elif any(line.startswith(FAILED_TEST_BEGIN) for line in stdout):
         score = 0
         output_started = False
-
         for line in stdout:
             if output_started and line.startswith(FAILED_TEST_CONTINUE):
                 output.append(line[len(FAILED_TEST_CONTINUE):])
