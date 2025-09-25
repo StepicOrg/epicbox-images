416c40499afdc79e8f238e4bb50f07f248a49ae6
Konstantin <seriuskosk@gmail.com>
2025-09-25 13:16:41 +0300

    Update Go 1.25.1 (#116)
    * Update Go 1.25.1
    
    * Update gin to 1.11.0
    
    * Fix indentation in go.mod and main.go

--- epicbox-go/main.go
+++ epicbox-go/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	_ "github.com/gin-gonic/gin"
-	_ "github.com/jmoiron/sqlx"
-	_ "github.com/mattn/go-sqlite3"
-	_ "gorm.io/driver/sqlite"
-	_ "gorm.io/gorm"
+        _ "github.com/gin-gonic/gin"
+        _ "github.com/jmoiron/sqlx"
+        _ "github.com/mattn/go-sqlite3"
+        _ "gorm.io/driver/sqlite"
+        _ "gorm.io/gorm"
 )
 
 func main() {
